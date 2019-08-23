2fbb6c2267241a7fe6f27624c410a9373d9d1da6
Mark Chen <37851975+marko-polo-cheno@users.noreply.github.com>
2019-08-23 09:51:42 -0400

    Update ball.py

--- ball.py
+++ ball.py
@@ -2,110 +2,89 @@ import math
 import random
 from PIL import Image, ImageDraw
 
-# Make image
-img_width = 799
-img_height = 599
-image = ImageDraw.Draw(Image.new("RGB", (img_width + 1, img_height + 1)))
+# canvas
+width = 899
+height = 899
+img = Image.new("RGB", (width + 1, height + 1))
+draw = ImageDraw.Draw(img)
 
-# Length of line
-distance = 20000
+# init
+cNum = random.randint(1, 7)
+max_Cize = int(min(width, height) / 4)
+min_Cize = 10
+xCircle = []
+yCircle = []
+rCircle = []
 
-# Obstacles setup
-num_of_circles = random.randint(1, 5)
-max_radius = int(min(img_width, img_height) / 4)
-min_radius = 10 
-
-# Create obstacles
-x_of_circles = []
-y_of_circles = []
-radius_of_circles = []
-
-# Place obstacles
-for cIndex in range(num_of_circles):
-
-    # Keep placing
+# create circles
+for indexCircle in range(cNum):
+    # circles cannot touch
     while(True):
-        circle_radius = random.randint(min_radius, max_radius)
-        circle_center_x = random.randint(circle_radius, img_width - circle_radius)
-        circle_center_y = random.randint(circle_radius, img_height - circle_radius)
-
+        circle_radius = random.randint(min_Cize, max_Cize)
+        x_pos = random.randint(circle_radius, width - circle_radius)
+        y_pos = random.randint(circle_radius, height - circle_radius)
         flag = True
-        
-        if cIndex > 0:
-            for everyCircle in range(cIndex):
-                if math.hypot(circle_center_x - x_of_circles[everyCircle], circle_center_y - y_of_circles[everyCircle]) < circle_radius + radius_of_circles[everyCircle]:
+        if indexCircle > 0:
+            for j in range(indexCircle):
+                if math.hypot(x_pos - xCircle[j], y_pos - yCircle[j]) < circle_radius + rCircle[j]:
                     flag = False
                     break
-        if flag: #==True
+        if flag == True:
             break
+    # draw circle when not touching
+    draw.ellipse((x_pos - circle_radius, y_pos - circle_radius, x_pos + circle_radius, y_pos + circle_radius))
+    xCircle.append(x_pos)
+    yCircle.append(y_pos)
+    rCircle.append(circle_radius)
 
-    # Add that obstacle
-    image.ellipse((circle_center_x - circle_radius, circle_center_y - circle_radius, circle_center_x + circle_radius, circle_center_y + circle_radius))
-    x_of_circles.append(circle_center_x)
-    y_of_circles.append(circle_center_y)
-    radius_of_circles.append(circle_radius)
-
-# Spawn ball in open space
-while(True):
-    x = float(random.randint(0, img_width))
-    y = float(random.randint(0, img_height))
-    flag = False
-    for cIndex in range(num_of_circles):
-        if math.hypot(x - x_of_circles[cIndex], y - y_of_circles[cIndex]) <= radius_of_circles[cIndex]:
+# spawn ball in open area
+flag = False
+while(flag != True):
+    x = float(random.randint(0, width))
+    y = float(random.randint(0, height))
+    # respawn until not in a circle
+    for indexCircle in range(cNum):
+        if math.hypot(x - xCircle[indexCircle], y - yCircle[indexCircle]) <= rCircle[indexCircle]:
             flag = True
-            break
-    if flag == False:
-        break
-    
-# Ball velocity
-vy = math.sin(math.pi * random.random() * 2)
-vx = math.cos(math.pi * random.random() * 2)
+        else :
+            flag = False
 
-pixdata = image.Image.load()##############################
+draw.ellipse((x, y, x+5, y+10))
+img.save("start.png", "PNG")
 
-for i in range(distance):
-    pixdata[x, y] == (255, 255, 255)######################
+
+# ball movement
+vector = 2.0 * math.pi * random.random()
+vx = math.cos(vector)
+vy = math.sin(vector)
+
+for steps in range(20000):
+    # add path
+    img.putpixel((int(x), int(y)), (255, 0, 0))
     xnew = x + vx
     ynew = y + vy
 
-    # reflection from the walls
-    if xnew < 0 or xnew > img_width:
+    # walls
+    if xnew < 0 or xnew > width:
         vx = -vx
         xnew = x
-    if ynew < 0 or ynew > img_height:
+    if ynew < 0 or ynew > height:
         vy = -vy
         ynew = y
 
-    # Bounce of circle
-    for i in range(num_of_circles):
-        if math.hypot(xnew - x_of_circles[i], ynew - y_of_circles[i]) <= radius_of_circles[i]:
-
-            # Angle change
-            v1 = math.atan2(ynew - y_of_circles[i], xnew - x_of_circles[i])
-            v2 = math.atan2(-vy, -vx)
-            vf = (v1 - v2) * 2 + v2
-
-
-            vy = math.sin(vf)
-            vx = math.cos(vf)
+    # bounce off circles
+    for indexCircle in range(cNum):
+        if math.hypot(xnew - xCircle[indexCircle], ynew - yCircle[indexCircle]) <= rCircle[indexCircle]:
+            angle_into_circle = math.atan2(ynew - yCircle[indexCircle], xnew - xCircle[indexCircle])
+            reverse_angle = math.atan2(-vy, -vx)
+            reflected_direction = reverse_angle + (angle_into_circle - reverse_angle) * 2
+            vy = math.sin(reflected_direction)
+            vx = math.cos(reflected_direction)
             xnew = x
             ynew = y
 
     x = xnew
     y = ynew
 
-import sys
-import urllib.request
-try:
-    # Add to folder
-    f = open('img.png','wb')
-    f.write(pixdata)
-    f.close()
-        
-except Exception:
-        print("No image")
-
-
-
-
-
+# final output
+img.save("ballPath.png", "PNG")
